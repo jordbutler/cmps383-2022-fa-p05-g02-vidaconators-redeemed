baa9d7e5dcc801735204c9273bb5ea7875fc4f38
dotbehrens <dotbehrens@gmail.com>
2022-09-23 20:59:50 -0500

    Add Home Screen

---
+++ FA22.P05.Web/ClientApp/src/Screens/Home.js
@@ -0,0 +1,13 @@
+
+import { WebAppBar } from "../Components/AppBar"
+import ProductCard from "../Components/ProductCard"
+export function HomeScreen() {
+    return (
+        <div color="#B5DDA4">
+
+            <WebAppBar />
+
+        </div>
+
+    )
+}
