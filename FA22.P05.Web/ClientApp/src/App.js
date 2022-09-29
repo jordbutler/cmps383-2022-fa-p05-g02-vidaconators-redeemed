49b40290afbf22c4d841e3596d238d4652344607
dotbehrens <dotbehrens@gmail.com>
2022-09-28 21:48:23 -0500

    add navigation drawer to app bar

--- FA22.P05.Web/ClientApp/src/App.js
+++ FA22.P05.Web/ClientApp/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { HomeScreen } from './Screens/Home'
+import { HomeScreen } from './Screens/Home';
+import { WebAppBar } from "../src/Components/AppBar";
+
+
 
 function App() {
+
+
   return (
     <div className="App">
-
+      <WebAppBar />
       <HomeScreen />
     </div>
   );
