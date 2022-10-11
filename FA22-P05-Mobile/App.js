8d9354006cc330bf531032c2cb509a2b85f666b0
dotbehrens <dotbehrens@gmail.com>
2022-10-11 13:36:58 -0500

    add navigation drawer; add login screen

--- FA22-P05-Mobile/App.js
+++ FA22-P05-Mobile/App.js
@@ -1,22 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Appbar } from "react-native-paper";
-import { MobileAppBar } from './Components/MobileAppBar';
-export default function App() {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {HomeScreen} from './Screens/HomeScreen';
+import {LoginScreen} from './Screens/LoginScreen';
+
+
+
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-     <MobileAppBar/>
-      <Text>TOCO</Text>
-    </View>
+    <Drawer.Navigator useLegacyImplementation>
+      <Drawer.Screen name="TOCO Games" component={HomeScreen} />
+      <Drawer.Screen name="Login  " component={LoginScreen} />
+    </Drawer.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
