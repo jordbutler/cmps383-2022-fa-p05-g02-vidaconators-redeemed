c8d4a184285a5c7a893e835e565e68a3d217970a
Dot Behrens <dotbehrens@gmail.com>
2022-10-26 20:10:39 -0500

    fix warnings

--- FA22-P05-Mobile/Screens/HomeScreen.js
+++ FA22-P05-Mobile/Screens/HomeScreen.js
@@ -1,18 +1,17 @@
 
 
 import * as React from "react";
-import { Dimensions } from "react-native";
-import { Text, View, StyleSheet, StatusBar } from "react-native";
-
+import { Text, View,  } from "react-native";
 import { useEffect, useState } from "react";
 import { ProductList } from "../Components/ProductsList";
 import axios from "axios";
-
-
+import { getActiveListings } from "../API/Listings";
+// import { ListingList } from "../Components/ListingList";
 export function HomeScreen() {
  const [products, setProducts] = useState([])
  const [isInitialRender, setIsInitialRender] = useState(true)
- 
+//  const [listings, setListings] = useState([])
+
  const url =  `https://cmps383-2022-fa-p05-g02-vidaconators-redeemed.azurewebsites.net/api/products`
 
  const getAllProducts = ()=> {
@@ -22,19 +21,24 @@ export function HomeScreen() {
          return response.data
      })
      .catch((response)=> {
-         
          console.error('there was a problem',response)
      })
  }
+ const handleGetActiveListings = () => {
+  setListings(getActiveListings())
+ }
+
  useEffect(()=> {
    if(isInitialRender === true){
      setIsInitialRender(false)
+    //  handleGetActiveListings()
      getAllProducts()
   }},[]);
 
   return (
     <View>
       <ProductList products={products}/>
+      {/* <ListingList listings={listings}/> */}
     </View>
   );
 }
--- FA22.P05.Web/Data/MigrateAndSeed.cs
+++ FA22.P05.Web/Data/MigrateAndSeed.cs
@@ -1,4 +1,9 @@
-﻿using FA22.P05.Web.Features.Authorization;
+﻿
+
+using FA22.P05.Web.Features.Authorization;
+using FA22.P05.Web.Features.ItemListings;
+using FA22.P05.Web.Features.Items;
+using FA22.P05.Web.Features.Listings;
 using FA22.P05.Web.Features.Products;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -16,9 +21,81 @@ public static class MigrateAndSeed
 
         await AddRoles(services);
         await AddUsers(services);
+        await AddListings(context);
     }
 
+    private static async Task AddListings(DataContext context)
+    {
+        var listings = context.Set<Listing>();
+        if (listings.Any())
+        {
+            return;
+        }
 
+        listings.Add(new Listing
+        {
+            Catagory = "Console",
+
+            Language = "English",
+
+            Name = "Nintendo Listing",
+
+            Country = "UK",
+
+            Description = "Nintendo Games",
+
+            Owner = context.Set<User>().FirstOrDefault(),
+
+            Price = 50.99m,
+
+            Year = 2022,
+
+            Publisher = "Nintendo",
+
+            OwnerId = 1,
+
+            StartUtc = DateTime.UtcNow,
+
+            EndUtc = DateTimeOffset.MaxValue,
+
+            Version = "N64",
+
+
+
+            ItemsForSale = new List<ItemListing>
+            {
+                new ItemListing
+                {
+
+                    Item = new Item
+                    {
+
+                        Condition = "Good",
+                        Product = context.Set<Product>().FirstOrDefault(),
+                        Owner = context.Set<User>().FirstOrDefault()
+
+
+                    }
+
+                },
+
+
+                new ItemListing
+                {
+                    Item = new Item
+                    {
+                        Condition = "Brand New",
+                        ProductId = 2,
+                        Owner = context.Set<User>().FirstOrDefault(),
+                    }
+                }
+
+            }
+
+        });
+        context.SaveChanges();
+
+    }
 
     private static void AddProducts(DataContext context)
     {
