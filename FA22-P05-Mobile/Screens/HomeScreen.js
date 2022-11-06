d51a342f45c4603535c9e5114e3e963867948e2a
Dot Behrens <dotbehrens@gmail.com>
2022-11-06 15:42:45 -0600

    fix warning

--- FA22-P05-Mobile/Screens/HomeScreen.js
+++ FA22-P05-Mobile/Screens/HomeScreen.js
@@ -1,7 +1,7 @@
 
 
 import * as React from "react";
-import { Text, View,Button  } from "react-native";
+import {  View, } from "react-native";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
