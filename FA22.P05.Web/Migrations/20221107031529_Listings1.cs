57fdbbe5c8a2291611ccf090ee432819e1ee1e80
Dot Behrens <dotbehrens@gmail.com>
2022-11-06 21:16:00 -0600

    fix migrations

---
+++ FA22.P05.Web/Migrations/20221107031529_Listings1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FA22.P05.Web.Migrations
+{
+    public partial class Listings1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Condition",
+                table: "Listing");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Condition",
+                table: "Listing",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
