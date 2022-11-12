b06128237cbfdc36ea7a3f3c83f4a8d733136b64
Dot Behrens <dotbehrens@gmail.com>
2022-11-12 13:57:51 -0600

    remove drawer navigation

--- FA22-P05-Mobile/App.js
+++ FA22-P05-Mobile/App.js
@@ -1,26 +1,28 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import {HomeScreen} from './Screens/HomeScreen';
-import {LoginScreen} from './Screens/LoginScreen';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { HomeScreen } from "./Screens/HomeScreen";
+import { LoginScreen } from "./Screens/LoginScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ListingScreen } from "./Screens/ListingScreen";
 
-
-const Drawer = createDrawerNavigator();
-
-export function DrawerNavigator() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name= "Toco Games" component={HomeScreen}  options={{headershown:false,  drawerLabel: "Home",}}/>
-      <Drawer.Screen name="Login" component={LoginScreen} />
-    </Drawer.Navigator>
-  );
-}
-
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigator/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headershown: false }}
+        />
+        <Stack.Screen
+          name="ListingScreen"
+          component={ListingScreen}
+          options={{ headershown: false }}
+        />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
