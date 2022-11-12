11fcb00b5d49642cd2c94f83db0e1d3f06536d79
Dot Behrens <dotbehrens@gmail.com>
2022-11-12 14:00:30 -0600

    format

--- FA22-P05-Mobile/Screens/HomeScreen.js
+++ FA22-P05-Mobile/Screens/HomeScreen.js
@@ -1,43 +1,39 @@
-
-
 import * as React from "react";
-import {  View, Text} from "react-native";
+import { View, Text } from "react-native";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 import { ListingList } from "../Components/ListingList";
 
+export function HomeScreen({ navigation }) {
+  const [isInitialRender, setIsInitialRender] = useState(true);
+  const [listings, setListings] = useState([]);
 
-export function HomeScreen({navigation}) {
- const [isInitialRender, setIsInitialRender] = useState(true)
- const [listings, setListings] = useState([])
+  const url = `https://cmps383-2022-fa-p05-g02-vidaconators-redeemed.azurewebsites.net/api/listings`;
 
- const url =  `https://cmps383-2022-fa-p05-g02-vidaconators-redeemed.azurewebsites.net/api/listings`
+  const handleGetActiveListings = () => {
+    const navigation = navigation;
+    return axios
+      .get(url)
+      .then((response) => {
+        setListings(response.data);
+        return response.data;
+      })
+      .catch((response) => {
+        console.error("there was a problem", response);
+      });
+  };
 
- 
- const handleGetActiveListings = () => {
-  const navigation= navigation
-  return axios.get(url).then((response)=> {
-    setListings(response.data)
-     return response.data
- })
- .catch((response)=> {
-     console.error('there was a problem',response)
- })
- }
-
- useEffect(()=> {
-   if(isInitialRender === true){
-     setIsInitialRender(false)
-     handleGetActiveListings()
-  
-  }},[]);
+  useEffect(() => {
+    if (isInitialRender === true) {
+      setIsInitialRender(false);
+      handleGetActiveListings();
+    }
+  }, []);
 
   return (
     <View>
-      <ListingList listings={listings}/>
+      <ListingList listings={listings} />
     </View>
   );
 }
-
-
