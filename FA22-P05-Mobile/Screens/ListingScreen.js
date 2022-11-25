9b2a8516a3ca9eab4ea1a26d9d3e0427d97a2525
Dot Behrens <dotbehrens@gmail.com>
2022-11-25 17:03:26 -0600

    create add to cart button; no function

--- FA22-P05-Mobile/Screens/ListingScreen.js
+++ FA22-P05-Mobile/Screens/ListingScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 
 import { useRoute } from "@react-navigation/native";
 import { ProductList } from "../Components/ProductList";
@@ -12,12 +12,19 @@ export function ListingScreen() {
     <View>
       <Text style={styles.productName}>{listing.name}</Text>
       <Text style={styles.productDescription}>{listing.startUtc}</Text>
-      <Text style={styles.productDescription}>{listing.price}</Text>
+      <Text style={styles.productDescription}>{`$${listing.price}`}</Text>
       <Text style={styles.productDescription}>{listing.publisher}</Text>
       <Text style={styles.productDescription}>{listing.version}</Text>
       <View style={styles.spacing} />
 
       <ProductList products={listing.itemsForSale}/>
+      <View style={styles.addToCartButtonContainer}>
+
+      <Button title="Add Listing To Cart" style={styles.addToCartButton} color='pink' onPress={()=> console.log('add')}>
+        {/* <Text>Add Listing To Cart</Text> */}
+      </Button>
+      </View>
+
     </View>
   );
 }
@@ -48,4 +55,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     width: 200,
   },
+  addToCartButtonContainer: {
+    color:'pink',
+    width: 300,
+    marginLeft:'auto',
+    marginRight:'auto',
+    borderRadius:25,
+    
+  }
 });
