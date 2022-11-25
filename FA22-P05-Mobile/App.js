dc93edc1f49fbc35c72267a1f548c1f8dc109cc6
Dot Behrens <dotbehrens@gmail.com>
2022-11-25 17:00:54 -0600

    add headermode; add MyCart Screen

--- FA22-P05-Mobile/App.js
+++ FA22-P05-Mobile/App.js
@@ -1,28 +1,40 @@
+import 'react-native-gesture-handler';
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { HomeScreen } from "./Screens/HomeScreen";
 import { LoginScreen } from "./Screens/LoginScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from '@react-navigation/stack';
 import { ListingScreen } from "./Screens/ListingScreen";
+import { MobileAppBar } from "./Components/MobileAppBar";
+import { CartScreen } from './Screens/CartScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
+  
+
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+        <MobileAppBar/>
+      <Stack.Navigator headerMode="none">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ headershown: false }}
-        />
+      
+          />
         <Stack.Screen
           name="ListingScreen"
           component={ListingScreen}
           options={{ headershown: false }}
-        />
+          />
+          <Stack.Screen
+          name="MyCart"
+          component={CartScreen}
+          options={{headershown: false}}/>
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+        
   );
 }
