c439a3fed1f7c3652fcf6e4b3f0708049c81b44d
Dot Behrens <dotbehrens@gmail.com>
2022-11-25 17:02:58 -0600

    create base cart screen

---
+++ FA22-P05-Mobile/Screens/CartScreen.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+import { useRoute } from "@react-navigation/native";
+import { ProductList } from "../Components/ProductList";
+
+export function CartScreen() {
+  const route = useRoute();
+//   const { listing } = route.params;
+
+  return (
+    <View>
+        <Text> MY CART </Text>
+      {/* <Text style={styles.productName}>{listing.name}</Text>
+      <Text style={styles.productDescription}>{listing.startUtc}</Text>
+      <Text style={styles.productDescription}>{`$${listing.price}`}</Text>
+      <Text style={styles.productDescription}>{listing.publisher}</Text>
+      <Text style={styles.productDescription}>{listing.version}</Text>
+      <Text>Total : {PriceTotal}</Text> */}
+      <View style={styles.spacing} />
+
+      {/* <ProductList products={listing.itemsForSale}/> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  spacing: {
+    height: 15,
+    marginBottom: 5,
+  },
+
+  productName: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    color: "#2A2D34",
+    fontSize: 20,
+    letterSpacing: 1,
+    lineHeight: 25,
+    textAlign: "center",
+    marginBottom: 10,
+    width: 200,
+  },
+  productDescription: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    color: "#2A2D34",
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 10,
+    width: 200,
+  },
+});
