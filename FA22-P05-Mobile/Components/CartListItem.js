76a61462d916798e5d935251d0275da387bb847a
Dot Behrens <dotbehrens@gmail.com>
2022-11-25 22:31:42 -0600

    add remove from cart button: non functioning

--- FA22-P05-Mobile/Components/CartListItem.js
+++ FA22-P05-Mobile/Components/CartListItem.js
@@ -1,11 +1,16 @@
 import * as React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { useContext } from "react";
+import { View, StyleSheet, Text, Button, Dimensions } from "react-native";
 import { ProductList } from "../Components/ProductList";
+import { GlobalContext } from "../GlobalContext";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export function CartListItem(props) {
+  const [state, setState] = useContext(GlobalContext);
   const listing = props.listing;
   return (
-    <View>
+    <View style={styles.listItemContainer}>
       <Text style={styles.productName}>{listing.name}</Text>
       <Text style={styles.productDescription}>{listing.startUtc}</Text>
       <Text style={styles.productDescription}>{`$${listing.price}`}</Text>
@@ -14,6 +19,14 @@ export function CartListItem(props) {
       <View style={styles.spacing} />
 
       <ProductList products={listing.itemsForSale} />
+      <View style={styles.addToCartButtonContainer}>
+        <Button
+          title="Remove From Cart"
+          color="#2c2c54"
+          textColor="#E0e0e0"
+          onPress={() => state.functions.removeItemFromCart(listing)}
+        />
+      </View>
       <View style={styles.addToCartButtonContainer}></View>
     </View>
   );
@@ -28,5 +41,41 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginLeft: "auto",
     marginRight: "auto",
+  },
+  priceContainer: {
+    marginTop: windowHeight / 15,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  productName: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: windowHeight / 35,
+    fontWeight: "bold",
+    color: "#2C2C54",
+    fontSize: 20,
+    letterSpacing: 1,
+    lineHeight: 25,
+    textAlign: "center",
+    marginBottom: 100,
+    width: 200,
+  },
+  productDescription: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    color: "#2A2D34",
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 10,
+    width: 200,
+  },
+  addToCartButtonContainer: {
+    marginTop: 10,
+    width: 300,
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: 25,
+    width: windowWidth / 1.8,
+    marginBottom: 14,
   },
 });
