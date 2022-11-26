c6d29781e9fb6663729f085ee93755af2e0c5415
Dot Behrens <dotbehrens@gmail.com>
2022-11-25 19:52:25 -0600

    add global context

--- FA22-P05-Mobile/App.js
+++ FA22-P05-Mobile/App.js
@@ -1,40 +1,39 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { HomeScreen } from "./Screens/HomeScreen";
 import { LoginScreen } from "./Screens/LoginScreen";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 import { ListingScreen } from "./Screens/ListingScreen";
 import { MobileAppBar } from "./Components/MobileAppBar";
-import { CartScreen } from './Screens/CartScreen';
-
+import { CartScreen } from "./Screens/CartScreen";
+import { GlobalContextProvider } from "./Screens/GlobalContext";
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-  
-
-    <NavigationContainer>
-        <MobileAppBar/>
-      <Stack.Navigator headerMode="none">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headershown: false }}
-      
-          />
-        <Stack.Screen
-          name="ListingScreen"
-          component={ListingScreen}
-          options={{ headershown: false }}
+    <GlobalContextProvider>
+      <NavigationContainer>
+        <MobileAppBar />
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headershown: false }}
           />
           <Stack.Screen
-          name="MyCart"
-          component={CartScreen}
-          options={{headershown: false}}/>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-        
+            name="ListingScreen"
+            component={ListingScreen}
+            options={{ headershown: false }}
+          />
+          <Stack.Screen
+            name="MyCart"
+            component={CartScreen}
+            options={{ headershown: false }}
+          />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GlobalContextProvider>
   );
 }
