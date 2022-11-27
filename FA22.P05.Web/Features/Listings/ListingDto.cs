36ef7a0b2b51ed83916a3ab15b43d44107ea7389
jordbutler <jordan.butler@selu.edu>
2022-11-27 11:36:59 -0600

    update

--- FA22.P05.Web/Features/Listings/ListingDto.cs
+++ FA22.P05.Web/Features/Listings/ListingDto.cs
@@ -1,4 +1,5 @@
-﻿using FA22.P05.Web.Features.Items;
+﻿using FA22.P05.Web.Features.ItemListings;
+using FA22.P05.Web.Features.Items;
 
 namespace FA22.P05.Web.Features.Listings;
 
@@ -17,5 +18,5 @@ public class ListingDto
     public string? Catagory { get; set; }
     public DateTimeOffset? StartUtc { get; set; }
     public DateTimeOffset? EndUtc { get; set; }
-    public IEnumerable<ItemDto> ItemsForSale { get; set; }
+    public virtual ICollection<ItemListing> ItemsForSale { get; set; } = new List<ItemListing>();
 }
