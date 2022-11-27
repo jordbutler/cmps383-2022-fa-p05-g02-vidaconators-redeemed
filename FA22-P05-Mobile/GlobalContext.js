0bde32b6f1fad46be5ee15ee3bba3b7e6037042b
Dot Behrens <dotbehrens@gmail.com>
2022-11-26 19:19:23 -0600

    remove checkfordups;temp fix

--- FA22-P05-Mobile/GlobalContext.js
+++ FA22-P05-Mobile/GlobalContext.js
@@ -1,29 +1,30 @@
 import React from "react";
-
 import { useState, createContext } from "react";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export const GlobalContext = createContext([{}, () => {}]);
 
 export function GlobalContextProvider(props) {
   const [cartContents, setCartContents] = useState([]);
+
   const [state, setState] = useState({
     functions: {
       addItemToCart: function (newContent) {
-        if (checkForDuplicates(cartContents, newContent) === false) {
-          console.log('hit2')
-          setCartContents(cartContents.push(newContent));
-        } else {
-          return "already in cart"
-        }
-
-        //Todo if already in cart if so notify user
+        let result;
+        setCartContents(cartContents.push(newContent));
+        result = "listingAdded";
+        //Todo check for duplicates and notify user
+        //Todo implement local storage save
+        return result;
       },
-      removeItemFromCart: function(item){
 
-      }
+      removeItemFromCart: function (item) {
+        setCartContents(cartContents.pop());
+        //todo remove specific item
+      },
 
-      // Todo function to remove from cart
-      // Todo function to get total price from contents
+      getSubTotal: function () {
+        // Todo function to get total price from contents
+      },
     },
     cartContents: cartContents,
   });
@@ -34,33 +35,22 @@ export function GlobalContextProvider(props) {
     </GlobalContext.Provider>
   );
 }
-function checkForDuplicates(cartContents, newContent) {
-  if (cartContents.length > 0) {
-    console.log('hit3')
-    cartContents.map((cartContent) => {
-      if (cartContent.id === newContent.id) {
-        return true;
-      } else return false;
-    });
-  } else {
-    return false;
-  }
-}
 
 const storeData = async (value) => {
   try {
-    const jsonValue = JSON.stringify(value)
-    await AsyncStorage.setItem('@storage_Key', jsonValue)
+    const jsonValue = JSON.stringify(value);
+    console.log("storing data");
+    await AsyncStorage.setItem("@storage_Key", jsonValue);
   } catch (e) {
     // saving error
   }
-}
+};
 
 const getData = async () => {
   try {
-    const jsonValue = await AsyncStorage.getItem('@storage_Key')
+    const jsonValue = await AsyncStorage.getItem("@storage_Key");
     return jsonValue != null ? JSON.parse(jsonValue) : null;
-  } catch(e) {
+  } catch (e) {
     // error reading value
   }
-}
+};
